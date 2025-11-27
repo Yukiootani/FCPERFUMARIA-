1244909f9b9d8cf651421f0a59341104c56e7253
Yukiootani <yukiocreditcard@hotmail.com>
2025-11-27 18:59:40 +0900

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+// ⚠️ IMPORTANTE: Estas chaves PRECISAM ser da FC PERFUMARIA
+const firebaseConfig = {
+    apiKey: "AIzaSyDxyqFLm08rqlaemlyYI9gQfrjvddPelJs",
+    authDomain: "fc-perfumaria-309fb.firebaseapp.com",
+    projectId: "fc-perfumaria-309fb",
+    storageBucket: "fc-perfumaria-309fb.firebasestorage.app",
+    messagingSenderId: "311935363734",
+    appId: "1:311935363734:web:a85cf6ccd93c67d594f0b7"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Isso garante que o App receba mensagens mesmo fechado
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Recebido em background:', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png',
+    data: { url: payload.notification.click_action || '/' }
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
