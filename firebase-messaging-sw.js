da53dccfc37ade3128bd5b9f2f3e312bf7d53d9b
Yukiootani <yukiocreditcard@hotmail.com>
2025-11-28 10:22:36 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
+// CHAVES DA FC PERFUMARIA
 const firebaseConfig = {
     apiKey: "AIzaSyDxyqFLm08rqlaemlyYI9gQfrjvddPelJs",
     authDomain: "fc-perfumaria-309fb.firebaseapp.com",
@@ -13,5 +14,30 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// NÃO interceptamos mais a mensagem.
-// Deixamos o sistema operacional mostrar do jeito dele.
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Mensagem recebida: ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png', // Ícone Novo
+    
+    // 🚨 TRUQUE DO 3 MARIAS (VIBRAÇÃO FORÇADA) 🚨
+    tag: 'push-alert-' + Date.now(), // Tag única para não agrupar
+    renotify: true, // Obriga o celular a tocar o som de novo
+    requireInteraction: true, // Fica na tela até clicar
+    
+    data: {
+        url: payload.notification.click_action || 'https://fcperfumaria.netlify.app'
+    }
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url || '/')
+  );
+});
